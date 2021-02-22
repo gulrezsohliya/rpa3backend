8e99e0317a5e890f20c7219b17ae8596ddf706de
kaushikdnath <kaushikdnath56@gmail.com>
2021-02-22 18:41:52 +0530

    user

---
+++ rpaoffline/src/rpa/dao/admin/AdminDao.java
@@ -0,0 +1,81 @@
+package rpa.dao.admin;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import rpa.models.master.Pageurls;
+import rpa.models.master.User;
+import rpa.models.master.UserPages;
+
+@Repository("AdminDao")
+@Transactional
+public class AdminDao {
+
+	// private ObjectMapper mapper = new ObjectMapper();
+	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	@Autowired
+	public void createTemplate(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public List<User> listUsers() {
+
+		List<User> list = null;
+		try {
+			String sql = "Select * From backend.userlogins Order by username";
+			list = (List<User>) jdbcTemplate.queryForList(sql, User.class);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("\n\nError in listUsers " + ex);
+		}
+		return (list != null) ? list : new LinkedList();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public List<User> listUsers(Integer usercode) {
+
+		List<User> list = null;
+		try {
+			String sql = "Select * From backend.userlogins Where usercode=? Order by username";
+			list = (List<User>) jdbcTemplate.queryForList(sql, User.class, usercode);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("\n\nError in listUsers " + ex);
+		}
+		return (list != null) ? list : new LinkedList();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public List<User> listUsers(String username) {
+
+		List<User> list = null;
+		try {
+			String sql = "Select * From backend.userlogins Where username=? Order by username";
+			list = (List<User>) jdbcTemplate.queryForList(sql, User.class, username);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("\n\nError in listUsers " + ex);
+		}
+		return (list != null) ? list : new LinkedList();
+	}
+
+}
--- rpaoffline/src/rpa/models/master/User.java
+++ rpaoffline/src/rpa/models/master/User.java
@@ -1,5 +1,92 @@
 package rpa.models.master;
 
+import java.sql.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
 public class User {
 
+	private Integer cellcode;
+	private Integer usercode;
+	private String username;
+	private String passwords;
+	private String fullname;
+	private String mobileno;
+	private String designation;
+	private String enabled;
+	@JsonSerialize(using = DateSerializer.class)
+	private Date entrydate;
+
+	public Integer getCellcode() {
+		return cellcode;
+	}
+
+	public void setCellcode(Integer cellcode) {
+		this.cellcode = cellcode;
+	}
+
+	public Integer getUsercode() {
+		return usercode;
+	}
+
+	public void setUsercode(Integer usercode) {
+		this.usercode = usercode;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswords() {
+		return passwords;
+	}
+
+	public void setPasswords(String passwords) {
+		this.passwords = passwords;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getEntrydate() {
+		return entrydate;
+	}
+
+	public void setEntrydate(Date entrydate) {
+		this.entrydate = entrydate;
+	}
 }
